082a55ef946bf1f8713dc912ee3f5ef8dbba143f
Adam Nigon <Adam.j.nigon@gmail.com>
2015-02-20 14:23:18 -0700

    Table View Controller implemented

---
+++ ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Nigon's on 2/20/15.
+//  Copyright (c) 2015 Adam Nigon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+@end
